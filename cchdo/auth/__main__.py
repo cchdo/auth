29ba0594e6eea5b2068ac193991c562be21503f0
Andrew Barna <abarna@gmail.com>
2023-08-29 00:52:11 +0000

    add edit_cruise command

--- cchdo/auth/__main__.py
+++ cchdo/auth/__main__.py
@@ -1,4 +1,34 @@
+import json
+
 import click
+
+from .session import session as s
+
+CCHDO_API = "https://cchdo.ucsd.edu/api/v1"
+CRUISE_API = f"{CCHDO_API}/cruise"
+
+
+def json_edit_loop(metadata):
+    human_json = json.dumps(metadata, indent=2, sort_keys=True)
+
+    try_again = True
+    while try_again:
+        edited = click.edit(text=human_json, extension=".json")
+
+        if edited is None:
+            raise click.Abort()
+
+        try:
+            json.loads(edited)
+            try_again = False
+        except ValueError:
+            click.confirm(
+                "Edited cruise was not valid json, would you like try fixing?",
+                abort=True,
+            )
+            human_json = edited
+
+    return json.loads(edited)
 
 
 @click.group()
@@ -19,6 +49,44 @@ def apikey(apikey):
         raise click.Abort
     write_apikey(apikey)
     click.echo(f"API Key written to {CONFIG_FILE}")
+
+
+@cli.command()
+@click.argument("expocode")
+def edit_cruise(expocode):
+    """Edit a cruise json in your $EDITOR"""
+
+    cruises = {c["expocode"]: c["id"] for c in s.get(CRUISE_API).json()["cruises"]}
+    try:
+        cruise_id = cruises[expocode]
+    except KeyError:
+        raise click.ClickException(f"Cruise not found: {expocode}")
+
+    click.echo(f"Loading cruise {cruise_id}")
+
+    cruise_json = s.get(f"{CRUISE_API}/{cruise_id}").json()
+
+    click.echo("Opening editor")
+
+    edited_metadata = json_edit_loop(cruise_json)
+
+    click.echo(f"PUTting to cruise: {cruise_id}")
+
+    while True:
+        r = s.put(f"{CRUISE_API}/{cruise_id}", json=edited_metadata)
+
+        if r.status_code != 201:
+            click.echo(f"Error message: {r.text}")
+            click.confirm(
+                "Edited cruise was rejected by the API, would you like try fixing?",
+                abort=True,
+            )
+            edited_metadata = json_edit_loop(edited_metadata)
+            continue
+
+        break
+
+    click.echo("Cruise Edit done!")
 
 
 if __name__ == "__main__":
